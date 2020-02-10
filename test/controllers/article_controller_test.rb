340a3279d5fcb9abd093868c65f47cae7dab38da
RaphaelCavalcante <raphael_fc_@live.com>
2020-02-10 15:19:04 -0300

    First layout modifications

--- test/controllers/article_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ArticleControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get article_index_url
-    assert_response :success
-  end
-
-end
